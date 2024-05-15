5408f571471fbfa2a27c1907f76342e035559dff
Xiaoze0525 <asd707305779@gmail.com>
2024-05-15 16:24:33 +0800

    v2

--- app/src/main/java/com/example/twedupuxiaoze/MainActivity.kt
+++ app/src/main/java/com/example/twedupuxiaoze/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.twedupuxiaoze
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import com.example.twedupuxiaoze.ui.theme.TwedupuxiaozeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +27,8 @@ class MainActivity : ComponentActivity() {
             TwedupuxiaozeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    PointerEvents()
                 }
             }
         }
@@ -27,17 +36,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TwedupuxiaozeTheme {
-        Greeting("Android")
+fun PointerEvents() {
+    var msg by remember { mutableStateOf("") }
+    Column {
+        Text(msg)
+        Box(Modifier
+                .fillMaxSize()
+                .background(Color.Yellow)
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+                        while (true) {
+                            val event = awaitPointerEvent()
+                            msg = "${event.type}, ${event.changes.first().position}"
+                        }
+                    }
+                })
     }
 }
